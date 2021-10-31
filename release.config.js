10c836b90a280a5e3d47b3e321d684437dad5e6e
jgambard <jmichel.gambard@gmail.com>
2021-10-31 09:24:28 +0100

    fix(ci): assets should work now

--- release.config.js
+++ release.config.js
@@ -8,8 +8,8 @@ module.exports = {
       "@semantic-release/github",
       {
         assets: [
-          { path: "build.zip", label: "build" },
-          { path: "coverage.zip", label: "coverage" },
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
         ],
       },
     ],
